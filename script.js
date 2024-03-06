4e2afd0af7ea94651590079bfd0cb9379a7145cf
javier noe cruz españa <javiernoecruzespana@gmail.com>
2024-03-06 17:34:14 -0600

    Primer commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function funcionUno(value){
+    alert("Función de los Integrantes del equipo: " + value);
+}
+
+function funcionDos(value) {
+    alert("Función de la Unidad I" + value);
+}
+
+function funcionTres(value) {
+    alert("Función de la Unidad II" + value);
+}
+
+function funcionCuatro(value) {
+    alert("Función de la Unidad III" + value);
+}
+
+function funcionCinco(value) {
+    alert("Función de la Unidad IV" + value);
+}
+
+function funcionSeis(value) {
+    alert("Función de la Unidad V" + value);
+}
